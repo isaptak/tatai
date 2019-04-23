5fe41d3470e46221bf7f9d113c1ea5493265a72a
Saptak M <43698360+isaptak@users.noreply.github.com>
2019-04-23 09:25:57 +0530

    Add files via upload

---
+++ search.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<title>Our Funky HTML Page</title>
+<meta name="description" content="Our first page">
+<meta name="keywords" content="html tutorial template">
+</head>
+<body>
+<?php echo $_POST["query"]; ?>
+Content goes here.
+</body>
+</html>
